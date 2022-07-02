b52f7da71fe4a31d4c88a332f0b45fcf90bc325f
BrokenBladee <47634664+BrokenBladee@users.noreply.github.com>
2022-07-02 23:07:32 +0200

    update url

--- LoLDamageCalculator/urls.py
+++ LoLDamageCalculator/urls.py
@@ -20,5 +20,5 @@ from dmgcalc.views import dmgcalc_function
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('dmgcalc/', include('dmgcalc.urls'))
-    path('dmgcalc/', dmgcalc)
+    path('dmgcalc/', dmgcalc_function())
 ]
